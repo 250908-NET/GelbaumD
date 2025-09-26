e99d1c8e9d3d1cb6b6a14c0a38f5828b826d4d18
Dean <deanscottg@gmail.com>
2025-09-26 09:01:50 -0400

    Added ReadMe

--- Project1/Barbershop.Api/Models/HaircutType.cs
+++ Project1/Barbershop.Api/Models/HaircutType.cs
@@ -12,16 +12,3 @@ namespace Barbershop.Models
     }
 }
 
-// {
-//     public enum HaircutType
-//     {
-//         Haircut,
-//         [Display(Name = "Haircut and Beard")]
-//         HaircutAndBeard,
-//         [Display(Name = "Shape Up")]
-//         ShapeUp,
-//         Shampoo,
-//         [Display(Name = "The Works")]
-//         TheWorks
-//     }
-// }
